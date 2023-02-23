14dbed7a351bb6cb213b9737a2e983b99335bd08
Liam Pugh <liam.pugh.009@gmail.com>
2023-02-23 19:28:47 +0000

    terraform created bucket

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -7,15 +7,28 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
 }
 
 resource "aws_s3_bucket" "filesync" {
-    bucket = "filesync-bucket"
+  bucket = "filesync-bucket"
 
-    tags = {
-        Name = "Filesync Bucket"
+  tags = {
+    Name = "Filesync Bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "filesync_bucket_acl" {
+  bucket = aws_s3_bucket.filesync.id
+
+  access_control_policy {
+    grant {
+      grantee {
+        id   = 926837967238
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
     }
+  }
 }
