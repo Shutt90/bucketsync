b36d07719fcb9ccf5ac6ea214f3bc586f57a02a7
Liam Pugh <liam.pugh.009@gmail.com>
2023-02-23 19:31:42 +0000

    changed to bucket owner

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -12,23 +12,16 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "filesync" {
-  bucket = "filesync-bucket"
+    bucket = "filesync-bucket"
 
-  tags = {
-    Name = "Filesync Bucket"
-  }
+    tags = {
+      Name = "Filesync Bucket"
+    }
 }
 
 resource "aws_s3_bucket_acl" "filesync_bucket_acl" {
   bucket = aws_s3_bucket.filesync.id
+  acl    = "private"
 
-  access_control_policy {
-    grant {
-      grantee {
-        id   = 926837967238
-        type = "CanonicalUser"
-      }
-      permission = "FULL_CONTROL"
-    }
-  }
+  expected_bucket_owner = 926837967238
 }
