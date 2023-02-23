f0669634b71a05088ba81a43a650424a02ddb6b2
Liam Pugh <liam.pugh.009@gmail.com>
2023-02-23 19:50:20 +0000

    Revert "changed to bucket owner"
    This reverts commit b36d07719fcb9ccf5ac6ea214f3bc586f57a02a7.

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -12,16 +12,23 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "filesync" {
-    bucket = "filesync-bucket"
+  bucket = "filesync-bucket"
 
-    tags = {
-      Name = "Filesync Bucket"
-    }
+  tags = {
+    Name = "Filesync Bucket"
+  }
 }
 
 resource "aws_s3_bucket_acl" "filesync_bucket_acl" {
   bucket = aws_s3_bucket.filesync.id
-  acl    = "private"
 
-  expected_bucket_owner = 926837967238
+  access_control_policy {
+    grant {
+      grantee {
+        id   = 926837967238
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+  }
 }
